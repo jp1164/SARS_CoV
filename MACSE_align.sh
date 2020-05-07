734836dbac1988b867b3644a0faa0eb27e7788d7
jp1164 <62770111+jp1164@users.noreply.github.com>
2020-05-07 18:28:17 -0400

    Update MACSE_align.sh

--- MACSE_align.sh
+++ MACSE_align.sh
@@ -1,1 +1,1 @@
-java -jar macse.jar -prog alignSequences -out_AA /Users/jamesp/Desktop/aa7.fas -out_NT /Users/jamesp/Desktop/nt7.fas -seq /Users/jamesp/Desktop/Molecular_Evolution_data/Coronavirus_Data/final_S_seq.fas
+java -jar macse_v2.03.jar -prog alignSequences -out_AA /Users/jamesp/Desktop/aa7.fas -out_NT /Users/jamesp/Desktop/nt7.fas -seq /Users/jamesp/Desktop/Molecular_Evolution_data/Coronavirus_Data/final_S_seq.fas
